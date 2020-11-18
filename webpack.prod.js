96f6a2e52e3aa500dcf12a70ac1e0c7813008e12
faniabdullah <tugasfani@gmail.com>
2020-11-19 01:15:17 +0700

    build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common');
-
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = merge(common, {
   mode: 'production',
   module: {
@@ -16,4 +16,7 @@ module.exports = merge(common, {
       },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
 });
